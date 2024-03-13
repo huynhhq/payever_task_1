0f43a847cde7a8895a162461a7025a8c505e07a7
Jax Hoang <huynhhq209@gmail.com>
2024-03-13 23:47:48 +0700

    init source

---
+++ main.js
@@ -0,0 +1,124 @@
+const svgNS = "http://www.w3.org/2000/svg";
+const pathsData = [
+  "M 555 10 L 190 10 C 10 10 10 0 10 190 L 10 356",
+  "M 555 30 L 190 30 C 110 30 110 110 110 190 L 110 356",
+  "M 555 50 L 280 52 C 210 50 210 90 210 210 L 210 356",
+  "M 555 70 L 370 70 C 305 70 305 90 305 305 L 305 356",
+
+  "M 500 90 C 500 356 395 90 395 356",
+  "M 560 90 C 560 356 485 90 485 356",
+
+  "M 580 90 V 356",
+
+  "M 600 90 C 600 356 675 90 675 356",
+  "M 660 90 C 660 356 775 90 775 356",
+
+  "M 600 10 L 965 10 C 1145 10 1145 0 1145 190 L 1145 356",
+  "M 600 30 L 965 30 C 1055 30 1055 30 1055 190 L 1055 356",
+  "M 600 50 L 875 50 C 965 53 965 114 965 190 L 965 356",
+  "M 600 70 L 785 70 C 875 70 875 70 875 190 L 875 356",
+];
+
+const logosData = [
+  "./assets/commerce_tools_ic.png",
+  "./assets/shopware.png",
+  "./assets/dan_domain_ic.png",
+  "./assets/plentymarkets_ic.png",
+  "./assets/ccv_shop_ic.png",
+  "./assets/magento_ic.png",
+  "./assets/shopify_ic.png",
+  "./assets/woocommerce_ic.png",
+  "./assets/opencart_ic.png",
+  "./assets/xt_ic.jpeg",
+  "./assets/oxid.jpeg",
+  "./assets/prestashop_ic.webp",
+  "./assets/jtl_ic.png",
+];
+
+const svgElement = document.getElementById("shopSystemSvg");
+
+// Function to create a path
+function createPath(id, d) {
+  const path = document.createElementNS(svgNS, "path");
+  path.setAttribute("id", id);
+  path.setAttribute("class", "path");
+  path.setAttribute("d", d);
+  svgElement.appendChild(path);
+}
+
+// Function to create a circle with animation
+function createAnimatedCircle(radius, pathId, duration) {
+  const circle = document.createElementNS(svgNS, "circle");
+  circle.setAttribute("class", "light");
+  circle.setAttribute("r", radius);
+  circle.style.filter = "url(#glow)"; 
+  
+  const animateMotion = document.createElementNS(svgNS, "animateMotion");
+  animateMotion.setAttribute("dur", duration);
+  animateMotion.setAttribute("repeatCount", "indefinite");
+
+  const mpath = document.createElementNS(svgNS, "mpath");
+  mpath.setAttributeNS(
+    "http://www.w3.org/1999/xlink",
+    "xlink:href",
+    `#${pathId}`
+  );
+
+  animateMotion.appendChild(mpath);
+  circle.appendChild(animateMotion);
+
+  svgElement.appendChild(circle);
+}
+
+function createPathWithImage(id, d, imageUrl) {
+  const path = document.createElementNS(svgNS, "path");
+  path.setAttribute("id", id);
+  path.setAttribute("class", "path");
+  path.setAttribute("d", d);
+  svgElement.appendChild(path);
+
+  const length = path.getTotalLength();
+  const point = path.getPointAtLength(length);
+
+  const foreignObject = document.createElementNS(svgNS, "foreignObject");
+  foreignObject.setAttribute("x", point.x - 40); // Adjust as needed
+  foreignObject.setAttribute("y", point.y - 40); // Adjust as needed
+  foreignObject.setAttribute("width", "80"); // Set the size of the div
+  foreignObject.setAttribute("height", "80");
+
+  const borderDiv = document.createElement("div");
+  borderDiv.className = "gradient-border";
+  // Create a div with styling
+  const div = document.createElement("div");
+  div.style.width = "95%"; // Make the div fill the foreignObject
+  div.style.height = "95%";
+  div.style.display = "flex";
+  div.style.alignItems = "center"; // Center the image vertically
+  div.style.justifyContent = "center"; // Center the image horizontally
+  div.style.borderRadius = "50%"; // To make it circular
+  div.style.backgroundColor = "#352831";
+
+  // Now, place an image at the end of the path
+  const image = document.createElement("img");
+  image.setAttribute("src", imageUrl);
+  image.setAttribute("x", point.x - 20); // Adjust x to align the image center to the path end
+  image.setAttribute("y", point.x - 20); // Adjust y to align the image center to the path end
+  image.setAttribute("height", "40"); // Set the image size as needed
+  image.setAttribute("width", "40");
+
+  borderDiv.appendChild(div);
+  div.appendChild(image);
+
+  // Append the div to the foreignObject
+  foreignObject.appendChild(borderDiv);
+
+  // Append the foreignObject to the SVG
+  svgElement.appendChild(foreignObject);
+}
+
+// Create paths and circles with animations
+pathsData.forEach((pathData, index) => {
+  createPath(`path${index}`, pathData);
+  createAnimatedCircle(3, `path${index}`, "4s");
+  createPathWithImage(`path${index}`, pathData, logosData[index]);
+});
